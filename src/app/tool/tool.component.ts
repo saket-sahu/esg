f226f23a6863564058b60489d945ca53b3f797df
saket-sahu <saket251291@gmail.com>
2022-11-21 10:11:24 +0530

    added highcharts

---
+++ src/app/tool/tool.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tool',
+  templateUrl: './tool.component.html',
+  styleUrls: ['./tool.component.scss']
+})
+export class ToolComponent {
+
+}
